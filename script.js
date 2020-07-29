baf0bc033837a577e946b5857f422fba57fd00cf
german <serigrafiatandil@gmail.com>
2020-07-28 21:28:40 -0300

    Desktop+Mobile+Nocturno listo, falta slider modo desktop

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const icon = document.getElementById("check");
 const img = document.getElementById('burgerIcon');
 
 icon.addEventListener("click",()=>{
-    nav.classList.toggle('nav-active');
-    
-    if (check.checked == true){
-        img.data = "./images/close.svg";
-      }else {
-        img.data = "./images/burger.svg";
-      } 
-})
-//--------------------------------------------------------XXX
 
+  nav.classList.toggle('nav-active');
+
+    if (check.checked == true && modoNoche.checked !== true){
+      img.src = "./images/close.svg";
+    }else if(check.checked == false && modoNoche.checked !== true) {
+      img.src = "./images/burger.svg";
+    }else if(check.checked !== true && modoNoche.checked == true) {
+      img.src = "./images/burger-noc.svg";
+    }else if(check.checked == true && modoNoche.checked == true){
+      img.src = "./images/button-close-noc.svg";
+    } 
+})
+
+//----------------------------------------------------------XXX
 
 //  BUSQUEDA DE GIFS ----------------------------------------
 const lupa = document.getElementById('search');
@@ -34,7 +39,7 @@ const tituGif = document.getElementById('tituloGif');
 input_gif.addEventListener("keyup",()=>{
 
   //AGREGO O QUITO LA CLASE SEGUN EL VALOR DE INPUT
-  if(input_gif.value !== ''){
+  if(input_gif.value !== '' && modoNoche.checked !== true){
 
     sugerencia.classList.add('displayBlocker');
     lupanueva.classList.add('displayBlocker');
@@ -43,15 +48,34 @@ input_gif.addEventListener("keyup",()=>{
     trendings[0].classList.add('displayNoner');
     lupa.setAttribute('src',"./images/close.svg");
 
-  }else{
+  }else if(input_gif.value == '' && modoNoche.checked !== true) {
 
     sugerencia.classList.remove('displayBlocker');
     lupanueva.classList.remove('displayBlocker');
     cutLinex.style.display = 'none';
     cutLine[0].classList.remove('displayBlocker');
     trendings[0].classList.remove('displayNoner');
-    lupa.setAttribute('src',"./images/icon-search.svg");
     grillaGifs.classList.remove('displayBlocker');
+    lupa.setAttribute('src',"./images/icon-search.svg");
+    
+
+  }else if(input_gif.value !== '' && modoNoche.checked == true){
+    sugerencia.classList.add('displayBlocker');
+    lupanueva.classList.add('displayBlocker');
+    lupanueva.setAttribute('src','./images/icon-search-mod-noc.svg');
+    cutLinex.style.display = 'unset';
+    cutLine[0].classList.add('displayBlocker');
+    trendings[0].classList.add('displayNoner');
+    lupa.setAttribute('src',"./images/button-close-noc.svg");
+
+  }else if(input_gif.value == '' && modoNoche.checked == true){
+    sugerencia.classList.remove('displayBlocker');
+    lupanueva.classList.remove('displayBlocker');
+    cutLinex.style.display = 'none';
+    cutLine[0].classList.remove('displayBlocker');
+    trendings[0].classList.remove('displayNoner');
+    grillaGifs.classList.remove('displayBlocker');
+    lupa.setAttribute('src',"./images/icon-search-mod-noc.svg");
   }
   
   //INICIA BUSQUEDA CON EL ENTER
@@ -61,15 +85,29 @@ input_gif.addEventListener("keyup",()=>{
 
   // ACCION DE CERRAR SUGERENCIAS Y RESETEAR CON LA CRUZ.
   lupa.addEventListener("click",()=>{
+    if(modoNoche.checked !== true){
 
-    sugerencia.classList.remove('displayBlocker');
-    lupanueva.classList.remove('displayBlocker');
-    cutLinex.style.display = 'none';
-    cutLine[0].classList.remove('displayBlocker');
-    trendings[0].classList.remove('displayNoner');
-    lupa.setAttribute('src',"./images/icon-search.svg");
-    input_gif.value = '';
-    grillaGifs.classList.remove('displayBlocker');
+      sugerencia.classList.remove('displayBlocker');
+      lupanueva.classList.remove('displayBlocker');
+      cutLinex.style.display = 'none';
+      cutLine[0].classList.remove('displayBlocker');
+      trendings[0].classList.remove('displayNoner');
+      lupa.setAttribute('src',"./images/icon-search.svg");
+      input_gif.value = '';
+      grillaGifs.classList.remove('displayBlocker');
+    }else{
+      sugerencia.classList.remove('displayBlocker');
+      lupanueva.classList.remove('displayBlocker');
+      lupanueva.setAttribute('src','./images/icon-search-mod-noc.svg');
+      cutLinex.style.display = 'none';
+      cutLine[0].classList.remove('displayBlocker');
+      trendings[0].classList.remove('displayNoner');
+      lupa.setAttribute('src',"./images/icon-search-mod-noc.svg");
+      input_gif.value = '';
+      grillaGifs.classList.remove('displayBlocker')
+
+    }
+
   })
 
   //GUARDO EL VALOR EN UNA VARIABLE
@@ -84,11 +122,72 @@ lupanueva.addEventListener('click',()=>{
 
 //----------------------------------------------------------XXX
 
+//MOUSE OVER BOTON DE CREAR GIFOS--------------------------->>>
+const makeGif = document.getElementById('makegif');
+const divgifCreatorBtn = document.getElementById('gif_creator');
 
+makeGif.addEventListener('mouseover',()=>{
+  if(modoNoche.checked !== true){
+    crearGif.setAttribute('src','./images/button-crear-gifo-white.svg');
+  }else{
+    crearGif.setAttribute('src','./images/button-crear-gifo-black.svg');
+  }
+})
+makeGif.addEventListener('mouseout',()=>{
+  if(modoNoche.checked !== true){
+    crearGif.setAttribute('src','./images/button-crear-gifo.svg');
+  }else{
+    crearGif.setAttribute('src','./images/button-crear-gifo-white.svg');
+  }
+})
 
+//----------CAMBIO A DARK THEME--------------------->>>
 
+//TOMO HOJA DE ESTILO LIGHT THEME(DEFAULT) Y LA GUARDO EN UNA CONSTANTE
+const theme = document.querySelector("#light-theme");
+//ELEMENTOS DEL NAV BAR
+const crearGif = document.getElementById('gifCreatorBtn');
+const modoNoche = document.getElementById('modoNoche');
+const logoNav = document.getElementById('logoNav');
+const btnMenu = document.querySelectorAll('.menubtn');
+const navlink = document.getElementsByClassName('nav-links');
+const burger = document.getElementById('burgerIcon');
 
+modoNoche.addEventListener('click',()=>{
+  cambioModo();
+  cambioIconos();
+})
 
+function cambioModo(){
+  if(theme.getAttribute("href") == "styles/main.css"){
+    theme.href = "styles/dark-main.css";
+  }else{
+    theme.href = "styles/main.css";
+  }
+}
+
+function cambioIconos(){
+
+  if (modoNoche.checked == true){
+
+    logoNav.setAttribute('src','./images/logo-desktop-modo-noc.svg');
+    btnMenu[0].textContent = 'Modo Diurno';
+    crearGif.setAttribute('src','./images/button-crear-gifo-noc.svg');
+    img.setAttribute('src',"./images/button-close-noc.svg");
+    //MAIN
+    lupa.setAttribute('src',"./images/icon-search-mod-noc.svg");
+
+  }else {
+
+    logoNav.setAttribute('src','./images/logo-desktop.svg');
+    btnMenu[0].textContent = 'Modo Nocturno';
+    crearGif.setAttribute('src','./images/button-crear-gifo.svg');
+    img.setAttribute('src',"./images/close.svg");
+    //MAIN
+    lupa.setAttribute('src',"./images/icon-search.svg");
+  }
+}
+//----------------------------------------------------------XXX
 
 
 var mediaqueryList = window.matchMedia("(max-width: 480px)");
